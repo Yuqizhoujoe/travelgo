dfa8455f9f0c5ee29bbe8aa7ce5f60a911ad94ed
damnjoejoe <kevinyuqi123@gmail.com>
2024-08-28 17:14:33 -0400

    update github action config need to add google cloud run for deployment later

--- grpc_client/grpc_client.go
+++ grpc_client/grpc_client.go
@@ -3,6 +3,7 @@ package grpc_client
 import (
 	"context"
 	"fmt"
+	"os"
 	"time"
 
 	pb "github.com/Yuqizhoujoe/user-service-proto/proto"
@@ -20,13 +21,18 @@ func InitGRPCClient() (pb.UserServiceClient, error) {
 		return client, nil
 	}
 
+	grpcServerURL := os.Getenv("GRPC_SERVER_URL")
+	if grpcServerURL == "" {
+		return nil, fmt.Errorf("GRPC_SERVER_URL environment variable not set")
+	}
+
 	// create a context with a timeout for the connection attempt
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
 
-	// establish gRPC connection to the UserService using grpc.WithTransportCredentials(insecure.NewCredentials())
+	// establish gRPC connection to the UserService
 	var err error
-	conn, err = grpc.DialContext(ctx, "localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
+	conn, err = grpc.DialContext(ctx, grpcServerURL, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to UserService: %v", err)
 	}
